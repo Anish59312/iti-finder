1c38dc39d477b19f627d04d4c1da7f4c8b9327c5
Anish <71869216+Anish59312@users.noreply.github.com>
2025-02-26 22:25:00 +0530

    Update main.js
    deleted  cors

--- Backend/main.js
+++ Backend/main.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const tradeRoutes = require('./routes/trades');
 const itiRoutes = require('./routes/iti.js');
